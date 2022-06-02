bbb90e112ea15f5ea7e147b0391eb5dab9acd45f
Thomas Claireau <thomas.claireau@gmail.com>
2022-06-02 16:17:45 +0200

    add menu

---
+++ content/global/menu.md
@@ -0,0 +1,5 @@
+---
+items:
+  - home
+  - about
+---
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import Header from '../components/Header';
-import { attributes } from '../content/pages/home.md';
 
 export default function Home() {
-  const { title } = attributes;
-
   return (
     <Header />
   );
