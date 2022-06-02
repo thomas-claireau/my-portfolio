4b06abb1fba8f35e96824ebf6f5c315ed1f03e33
Thomas Claireau <thomas.claireau@gmail.com>
2022-06-02 11:20:15 +0200

    config netlify cms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import { useAppContext } from '../AppContext';
 
+import { attributes, react as HomeContent } from '../content/home.md';
+
 export default function Home() {
   const { i18n } = useAppContext();
 
+  const { title } = attributes;
+
   return (
     <div className="h-[100vh] w-full flex items-center justify-center">
-      <h1 className="text-4xl font-bold">{i18n?.h1}</h1>
+      <h1 className="text-4xl font-bold">{title}</h1>
     </div>
   );
 }
